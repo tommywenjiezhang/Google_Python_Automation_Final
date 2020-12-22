f4c15bd24f5b4890f3f77e4e19cbfe2e936ea5b4
tommywenjiezhang <tommywenjiezhang@gmail.com>
2020-12-22 08:52:22 +0000

    finished Working with supplier images" quit()

--- changeImage.py
+++ changeImage.py
@@ -5,8 +5,9 @@ import glob, os
 
 size = 600, 400
 
-for infile in glob.glob("*.jpg"):
+for infile in glob.glob("./supplier-data/images/*.tiff"):
     file, ext = os.path.splitext(infile)
-        im = Image.open(infile)
-            im.thumbnail(size)
-                im.save(file + ".thumbnail", "JPEG")
+    im = Image.open(infile)
+    im = im.convert('RGB')
+    im.thumbnail(size)
+    im.save(file + ".jpeg", "JPEG", quality=100)
