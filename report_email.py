302da35b57d949b859637b345c4d4d162bdc659b
tommywenjiezhang <tommywenjiezhang@gmail.com>
2020-12-27 06:41:36 -0500

    Send report through email

--- report_email.py
+++ report_email.py
@@ -5,6 +5,13 @@ import os
 from reports import generate_report
 from emails import generate_email,send
 
+
+sender = 'automation@example.com'
+to = 'username@example.com'
+email_body = "Upload Completed - Online Fruit Store", "All fruits are uploaded to our website successfully. A detailed list is attached to this email."
+attachment = '/tmp/processed.pdf'
+input_path = "./"
+
 def getNameAndWeight(input_path, sep):
     output = []
     for f in os.listdir(input_path):
@@ -18,13 +25,13 @@ def getNameAndWeight(input_path, sep):
 
 
 if __name__ == "__main__":
-    output = getNameAndWeight("./google_project","<br/>")
+    output = getNameAndWeight(input_path,"<br/>")
     today = date.today()
     today_str = today.strftime("%d %B, %Y")
     process_str = "Processed Update on {}".format(today_str)
     output_para = "".join(output)
-    generate_report('/tmp/processed.pdf',process_str,output_para)
-    email = generate_email('automation@example.com',  'username@example.com', "Upload Completed - Online Fruit Store", "All fruits are uploaded to our website successfully. A detailed list is attached to this email.",'/tmp/processed.pdf')
+    generate_report(attachment,process_str,output_para)
+    email = generate_email(sender,to ,email_body ,attachment)
     send(email)
 
 
